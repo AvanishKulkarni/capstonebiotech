e7d138409520a5a771ebfff5ff3c2827f931e1cf
NoxiousFumes <55012494+NoxiousFumes@users.noreply.github.com>
2022-03-14 14:23:05 -0400

    humidity

--- SensorArray/SensorArray.ino
+++ SensorArray/SensorArray.ino
@@ -3,24 +3,33 @@
 
 dht11 DHT11;
 
+// 250  100%
+// 1050 0%  
+
 void setup()
 {
   Serial.begin(9600);
- 
+  
 }
 
 void loop()
 {
-  Serial.println();
-
   int chk = DHT11.read(DHT11PIN);
+  int moist = analogRead(A5);
+  float moistPercent = basedMap(moist, 1050, 250, 0, 1);
 
-  Serial.print("Humidity (%): ");
-  Serial.println((float)DHT11.humidity, 2);
+  Serial.print(yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy\ ty, 2);
 
-  Serial.print("Temperature (C): ");
+  Serial.print("T");
   Serial.println((float)DHT11.temperature, 2);
 
-  delay(2000);
+  Serial.print("S");
+  Serial.println(moistPercent);
 
+  delay(2000);
+  
+}
+
+float basedMap(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
